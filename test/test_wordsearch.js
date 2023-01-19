ef49d97caddddb435c074d195e68c0023982339e
JunseongKim0105 <juns.kim0105@gmail.com>
2023-01-18 19:52:47 -0500

    pairworking

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -32,7 +32,24 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
-
     assert.isTrue(result);
+  });
+  it("vertical test", function() { 
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['B', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['I', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['T', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['E', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['S', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ABILITIES')
+    assert.isTrue(result);
+  });
+  it("should return false for empty array", function() {
+    const result = wordSearch([], 'SEINFELD')
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+function transpose(matrix) {
+  return matrix[0].map((col, c) => matrix.map((row, r) => matrix[r][c]));
 }
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  const transposedGraph = transpose(letters);
+  const verticalJoin = transposedGraph.map((ls) => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const horizontalJoin = letters.map((ls) => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
